0a9c3030d15cdb473de8052e19bfacdd4cb6911b
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-11-14 20:08:13 -0600

    fix: clarify behavior of dry-run
    * fix: specify current branch during dry-run
    
    * fix: use GITHUB_HEAD_REF
    
    * chore: add dry run workflow
    
    * chore: use debug during dry run
    
    * fix: use GITHUB_HEAD_REF
    
    * fix: use absolute path for outputs.sh
    
    * fix: update outputs.sh
    
    * fix: update VERSION_RELEASED logic
    
    * fix: correct if syntax for bash
    
    * fix: correct variables for output
    
    * chore: publish image from test pipeline
    
    * chore: standardize steps between dry-run and release
    
    * fix: specify branch for dry-run instead of debug
    
    * fix: reset branch_arg at correct point
    
    * chore: use pre-built image

--- outputs.sh
+++ outputs.sh
@@ -2,11 +2,14 @@
 
 LAST_VERSION=$1
 NEXT_VERSION=$2
-VERSION_RELEASED=$3
+if [ "${LAST_VERSION}" == "${NEXT_VERSION}" ]
+then VERSION_RELEASED=false
+else VERSION_RELEASED=true
+fi
 
 echo $LAST_VERSION
 echo "last-version=$LAST_VERSION" >> $GITHUB_OUTPUT
 echo $NEXT_VERSION
-echo "last-version=$NEXT_VERSION" >> $GITHUB_OUTPUT
+echo "next-version=$NEXT_VERSION" >> $GITHUB_OUTPUT
 echo $VERSION_RELEASED
-echo "last-version=$VERSION_RELEASED" >> $GITHUB_OUTPUT
+echo "version-released=$VERSION_RELEASED" >> $GITHUB_OUTPUT
